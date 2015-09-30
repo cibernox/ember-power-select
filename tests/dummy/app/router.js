d6fd571b38548330b14f7d9ed971aef043101303
cibernox <miguel.camba@gmail.com>
2015-09-30 18:26:02 +0100

    Add dummy templates for all entries in the docs

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,12 +7,27 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('docs', function() {
+    // GETTING STARTED
     // index.hbs is "Overview"
     this.route('installation');
     this.route('how-to-use-it');
     this.route('action-handling');
     this.route('groups');
     this.route('multiple-selection');
+
+    // BASIC CUSTOMIZATION
+    this.route('list-items');
+    this.route('empty-list');
+    this.route('selected-element');
+    this.route('messages');
+    this.route('filtering');
+    this.route('styles');
+
+    // ADVANCED CUSTOMIZATION
+    this.route('partials');
+    this.route('asynchronous-search');
+    this.route('roll-your-own-template');
+
   });
   this.route('cookbook');
   this.route('legacy-demo');
