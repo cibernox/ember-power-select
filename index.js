a2ab5b0f3b25496619be8b2d7b1ee882ff50657b
cibernox <miguel.camba@gmail.com>
2015-10-02 09:19:42 +0100

    Prepare sass styles

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 /* jshint node: true */
 'use strict';
 
+// var path = require('path');
+
 module.exports = {
   name: 'ember-power-select',
   contentFor: function(type, config) {
     if (config.environment !== 'test' && type === 'body-footer') {
       return '<div id="ember-power-select-wormhole"></div>';
     }
-  }
+  },
+
+  included: function(app) {
+    this._super.included(app);
+    // this.app.registry.availablePlugins['ember-cli-sass'] --->' 4.2.1',
+    // I can use this to determine if the consumer app is using sass
+    // and if so take a different approch.
+  },
+
+  // treeForStyles: function(){
+  //   debugger;
+  //   var tree = new Funnel(this.treePaths['addon-styles'], {
+  //     srcDir: '/',
+  //     destDir: '/app/styles'
+  //   });
+  //   return tree;
+  // }
 };
  
