ad70f8e86d1a5c1a69b2985e62c4da6730221808
cibernox <miguel.camba@gmail.com>
2015-10-02 20:44:39 +0100

    Created script the compiles to vendor

---
+++ compile-css.js
@@ -0,0 +1,11 @@
+/* global require, module */
+var sass = require('node-sass');
+var fs = require('fs');
+var path = require('path');
+
+var inputFile = path.join(__dirname, 'app', 'styles', 'ember-power-select.scss');
+var outputFile = path.join(__dirname, 'vendor', 'ember-power-select.css');
+var buf = fs.readFileSync(inputFile, "utf8");
+var result = sass.renderSync({ data: buf });
+
+fs.writeFileSync(outputFile, result.css);
