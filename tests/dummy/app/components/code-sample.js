76221cb009a1123c9ef7dcf2625aceca3f34f7bc
cibernox <miguel.camba@gmail.com>
2015-10-03 19:55:06 +0100

    Basic syntax highlighting.
    Not very good IMO, but it’s better than nothing.

--- tests/dummy/app/components/code-sample.js
+++ tests/dummy/app/components/code-sample.js
@@ -4,5 +4,15 @@ export default Ember.Component.extend({
   tagName: 'article',
   classNames: ['code-sample'],
   tabs: ['template', 'javascript', 'result'],
-  activeTab: 'template'
+  activeTab: 'template',
+
+  // Hooks
+  didInsertElement() {
+    this._super(...arguments);
+    if (window.hljs) {
+      this.$('pre').each(function(i, block) {
+        window.hljs.highlightBlock(block, { languages: ['js', 'hbs'] });
+      });
+    }
+  }
 });
