23e62e3049a9a49265aa3b4c310302f7f1e6250c
cibernox <miguel.camba@gmail.com>
2015-10-08 00:18:51 +0100

    Released v0.2.0

--- compile-css.js
+++ compile-css.js
@@ -6,6 +6,9 @@ var path = require('path');
 var inputFile = path.join(__dirname, 'app', 'styles', 'ember-power-select.scss');
 var outputFile = path.join(__dirname, 'vendor', 'ember-power-select.css');
 var buf = fs.readFileSync(inputFile, "utf8");
-var result = sass.renderSync({ data: buf });
+var result = sass.renderSync({
+  data: buf,
+  includePaths: ['node_modules/ember-basic-dropdown/app/styles/']
+});
 
 fs.writeFileSync(outputFile, result.css);
