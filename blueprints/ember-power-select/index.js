0358fabb5cc033c38704c9bebb668ddf56d451bf
Miguel Andrade <jmandrade0@gmail.com>
2015-10-13 10:16:18 +0100

    blueprint fix workaround. closes #36

---
+++ blueprints/ember-power-select/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  }
+};
