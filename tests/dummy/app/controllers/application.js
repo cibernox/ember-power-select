c903b23f0615dc95a968099352788f933da86a8f
cibernox <miguel.camba@gmail.com>
2015-10-16 01:59:01 +0100

    Easter egg

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
+const options = [
+  ['I\'m', 'just', 'a logo'],
+  ['I', 'don\'t', 'work'],
+  ['Are', 'you', 'serius?'],
+  ['No', 'really.', 'Stop'],
+  ['La la', 'la la', 'I don\'t hear you!'],
+  ['Ok', 'I\'m done', 'with you']
+];
+
 export default Ember.Controller.extend({
+  mainSelectOptions: options[0],
+  mainSelected: 'foo',
+
+  actions: {
+    changeOptions() {
+      const index = options.indexOf(this.get('mainSelectOptions')) + 1;
+      if (index === options.length) {
+        this.set('disabled', true);
+      } else {
+        this.set('mainSelectOptions', options[index]);
+      }
+    }
+  }
 });
