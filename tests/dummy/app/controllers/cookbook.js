47ae23e156ac837d2dd6b9ea8d49006d8dbb1a7b
cibernox <miguel.camba@gmail.com>
2015-11-01 09:30:37 +0000

    Add mobile menu to cookbook

---
+++ tests/dummy/app/controllers/cookbook.js
@@ -0,0 +1,42 @@
+  import Ember from 'ember';
+
+const { computed } = Ember;
+const { service } = Ember.inject;
+
+const groupedSections = [
+  {
+    groupName: 'Basic recipes',
+    options: [
+      { route: 'cookbook.index',            text: 'System-wide config' },
+      { route: 'cookbook.bootstrap-theme',  text: 'Installation' },
+    ]
+  },
+  {
+    groupName: 'Advanced recipes',
+    options: [
+      { route: 'cookbook.navigable-select', text: 'Navigable select' },
+    ]
+  }
+];
+
+export default Ember.Controller.extend({
+  routing: service('-routing'),
+  groupedSections: groupedSections,
+
+  currentSection: computed('routing.currentRouteName', function() {
+    const currentRouteName = this.get('routing.currentRouteName');
+    for (let group of groupedSections) {
+      for (let section of group.options){
+        if (section.route === currentRouteName) {
+          return section;
+        }
+      }
+    }
+  }),
+
+  actions: {
+    visit(section) {
+      this.transitionToRoute(section.route);
+    }
+  }
+});
