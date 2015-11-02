5a75028570adbb1f85d10a723f3256e5f3890126
cibernox <miguel.camba@gmail.com>
2015-11-02 22:25:04 +0000

    Try to reproduce bug with ember-data

--- tests/dummy/app/mirage/config.js
+++ tests/dummy/app/mirage/config.js
@@ -1,4 +1,6 @@
 export default function() {
+
+  this.timing = 4000;
   this.get('/users', function(db) {
     return {
       data: db.users.map(attrs => ({type: 'users', id: attrs.id, attributes: attrs }))
--- tests/dummy/app/mirage/scenarios/default.js
+++ tests/dummy/app/mirage/scenarios/default.js
@@ -1,7 +1,7 @@
-export default function(/* server */) {
+export default function(server) {
 
   // Seed your development database using your factories. This
   // data will not be loaded in your tests.
 
-  // server.createList('contact', 10);
+  server.createList('user', 10);
 }
