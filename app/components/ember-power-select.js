83f78234bf4a0617df154d1e59c19c0fbcfa504a
cibernox <miguel.camba@gmail.com>
2015-11-11 19:38:24 +0000

    Remove deprecated {{ember-power-select}}

--- app/components/ember-power-select.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import EmberPowerSelectComponent from 'ember-power-select/components/power-select';
-
-export default EmberPowerSelectComponent.extend({
-  init() {
-    this._super(...arguments);
-    Ember.deprecate('The name of the component has been shortened from {{ember-power-select}} to just {{power-select}}. The old component will be removed in the next major version.', false);
-  }
-});
