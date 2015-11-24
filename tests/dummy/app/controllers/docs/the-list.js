f4742d7be2ba56e698b54e0fa518e61f24ac3298
cibernox <miguel.camba@gmail.com>
2015-11-24 00:04:28 +0000

    Document how to disable specific options

--- tests/dummy/app/controllers/docs/the-list.js
+++ tests/dummy/app/controllers/docs/the-list.js
@@ -6,10 +6,21 @@ function generatePromise() {
   });
 }
 
+const countries = [
+  { name: 'United States',  code: 'US', population: 321853000 },
+  { name: 'Spain',          code: 'ES', population: 46439864 },
+  { name: 'Portugal',       code: 'PT', population: 10374822, disabled: true },
+  { name: 'Russia',         code: 'RU', population: 146588880, disabled: true },
+  { name: 'Latvia',         code: 'LV', population: 1978300 },
+  { name: 'Brazil',         code: 'BR', population: 204921000, disabled: true },
+  { name: 'United Kingdom', code: 'GB', population: 64596752 },
+];
+
 export default Ember.Controller.extend({
   names: ['Stefan', 'Miguel', 'Tomster', 'Pluto'],
   emptyList: [],
   promise: null,
+  countries,
 
   actions: {
     refreshCollection() {
