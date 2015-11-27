ec7dee22956dd49e356dbeaf9b1faf3772ae79ef
cibernox <miguel.camba@gmail.com>
2015-11-27 20:25:32 +0000

    Add docs for new feature

--- tests/dummy/app/controllers/docs/action-handling.js
+++ tests/dummy/app/controllers/docs/action-handling.js
@@ -3,10 +3,19 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   cities: ['Barcelona', 'London', 'New York', 'Porto'],
   destination: 'London',
+  selectedCities: [],
 
   actions: {
     chooseDestination(city) {
       this.set('destination', city);
+    },
+
+    handleKeydown(dropdown, e) {
+      if (e.keyCode !== 13) { return; }
+      let text = e.target.value;
+      if (text.length > 0 && this.get('cities').indexOf(text) === -1) {
+        this.get('selectedCities').pushObject(text);
+      }
     }
   }
 });
