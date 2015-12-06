f1252932ee12d347fb2730ff2a0b4bb450c0194c
cibernox <miguel.camba@gmail.com>
2015-12-06 20:23:14 +0000

    Allow to open/close select with the opened property

--- tests/integration/components/power-select-test.js
+++ tests/integration/components/power-select-test.js
@@ -2347,7 +2347,62 @@ test('the list of options can be customized using optionsComponent', function(as
 });
 
 /**
-12 - Development assertions
+13 - Open/Close vía property
+*/
+
+moduleForComponent('ember-power-select', 'Integration | Component | Ember Power Select (Open/close via property)', {
+  integration: true
+});
+
+test('the select can be rendered already opened by passing `opened=true`', function(assert) {
+  assert.expect(1);
+
+  this.numbers = numbers;
+  this.render(hbs`
+    {{#power-select options=numbers onchange=(action (mut foo)) opened=true as |option|}}
+      {{option}}
+    {{/power-select}}
+  `);
+
+  assert.equal($('.ember-power-select-dropdown').length, 1, 'Dropdown is opened');
+});
+
+test('It can be opened and closed by toggling the `opened` property', function(assert) {
+  assert.expect(3);
+
+  this.numbers = numbers;
+  this.opened = false;
+  this.render(hbs`
+    {{#power-select options=numbers onchange=(action (mut foo)) opened=opened as |option|}}
+      {{option}}
+    {{/power-select}}
+  `);
+
+  assert.equal($('.ember-power-select-dropdown').length, 0, 'Dropdown is closed');
+  Ember.run(() => this.set('opened', true));
+  assert.equal($('.ember-power-select-dropdown').length, 1, 'Dropdown is opened');
+  Ember.run(() => this.set('opened', false));
+  assert.equal($('.ember-power-select-dropdown').length, 0, 'Dropdown is closed again');
+});
+
+test('when it is opened/closed and the `opened` property is multable, it gets updated', function(assert) {
+  assert.expect(2);
+
+  this.numbers = numbers;
+  this.opened = false;
+  this.render(hbs`
+    {{#power-select options=numbers onchange=(action (mut foo)) opened=opened as |option|}}
+      {{option}}
+    {{/power-select}}
+  `);
+
+  assert.ok(!this.get('opened'), 'The opened property is false');
+  Ember.run(() => this.$('.ember-power-select-trigger').click());
+  assert.ok(this.get('opened'), 'The opened property is true now');
+});
+
+/**
+13 - Development assertions
 */
 
 moduleForComponent('ember-power-select', 'Integration | Component | Ember Power Select (Assertions)', {
