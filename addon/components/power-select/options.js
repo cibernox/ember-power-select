d8734e6413a07ee6ba69fedbc9e2d77b7a9c3e98
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-12-11 21:36:57 +0000

    Add listbox, option and combobox roles

--- addon/components/power-select/options.js
+++ addon/components/power-select/options.js
@@ -4,5 +4,7 @@ import layout from '../../templates/components/power-select/options';
 // TODO: Do I really need a component? A recursive template would do ...
 export default Ember.Component.extend({
   layout: layout,
-  tagName: 'ul'
+  tagName: 'ul',
+  attributeBindings: ['role'],
+  role: 'listbox'
 });
