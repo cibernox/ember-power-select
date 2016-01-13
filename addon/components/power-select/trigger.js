d8dc645d034b96a0b7edf3f4ecf9302a1241a5ad
cibernox <miguel.camba@gmail.com>
2016-01-13 10:10:28 +0000

    Remove unnecesary intermediate action

--- addon/components/power-select/trigger.js
+++ addon/components/power-select/trigger.js
@@ -3,12 +3,5 @@ import layout from '../../templates/components/power-select/trigger';
 
 export default Ember.Component.extend({
   layout: layout,
-  tagName: '',
-
-  // Actions
-  actions: {
-    clear(e) {
-      this.get('select.actions.select')(null, e);
-    }
-  }
+  tagName: ''
 });
