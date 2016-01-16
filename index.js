6c31e88b7c97e91073ebea9ef5b3a42566891355
cibernox <miguel.camba@gmail.com>
2016-01-16 09:47:20 +0000

    Allow ember-power-select to be invoked transitively from another addon
    Also, the included hook is idempotent. The second time that is called is a noop

--- index.js
+++ index.js
@@ -6,16 +6,20 @@
 module.exports = {
   name: 'ember-power-select',
 
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-    // Don't include the precompiled css file if the user uses ember-cli-sass
-    if (!app.registry.availablePlugins['ember-cli-sass']) {
-      var addonConfig = app.options['ember-power-select'];
+  included: function(appOrAddon) {
+    let app = appOrAddon.app || appOrAddon;
+    if (!app.__emberPowerSelectIncludedInvoked) {
+      app.__emberPowerSelectIncludedInvoked = true;
+      this._super.included.apply(this, arguments);
+      // Don't include the precompiled css file if the user uses ember-cli-sass
+      if (!app.registry.availablePlugins['ember-cli-sass']) {
+        var addonConfig = app.options['ember-power-select'];
 
-      if (!addonConfig || !addonConfig.theme) {
-        app.import('vendor/ember-power-select.css');
-      } else {
-        app.import('vendor/ember-power-select-'+addonConfig.theme+'.css');
+        if (!addonConfig || !addonConfig.theme) {
+          app.import('vendor/ember-power-select.css');
+        } else {
+          app.import('vendor/ember-power-select-'+addonConfig.theme+'.css');
+        }
       }
     }
   },
