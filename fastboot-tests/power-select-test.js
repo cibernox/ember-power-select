e0320030df01e16049719d59c120cb728e3696f9
cibernox <miguel.camba@gmail.com>
2016-02-03 13:14:19 +0000

    Add a couple more assertions

--- fastboot-tests/power-select-test.js
+++ fastboot-tests/power-select-test.js
@@ -5,13 +5,15 @@ var jsdom   = require("jsdom").jsdom;
 QUnit.module('Fastboot render');
 
 QUnit.test('it renders', function(assert) {
-  assert.expect(3);
+  assert.expect(5);
 
-  return request('http://localhost:3000').then(function(response) {
+  return request('http://localhost:3000/fastboot-test').then(function(response) {
     assert.equal(response.statusCode, 200, 'Request is successful');
     assert.equal(response.headers["content-type"], "text/html; charset=utf-8", 'Content type is correct');
     var document = jsdom(response.body).defaultView.document;
     var eps = document.querySelector('.ember-power-select');
     assert.ok(eps, 'The select has been rendered');
+    assert.ok(eps.querySelector('.ember-power-select-trigger'), 'It contains a trigger');
+    assert.equal(eps.textContent.trim(), '', 'Nothing is selected');
   });
 });
