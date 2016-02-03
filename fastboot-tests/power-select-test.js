45b4fac1aaf595da52a727b458ade4d184136d69
cibernox <miguel.camba@gmail.com>
2016-02-03 11:11:07 +0000

    Works!

--- fastboot-tests/power-select-test.js
+++ fastboot-tests/power-select-test.js
@@ -1,20 +1,17 @@
 var RSVP    = require('rsvp');
 var request = RSVP.denodeify(require('request'));
-var jsdom   = require("jsdom");
+var jsdom   = require("jsdom").jsdom;
 
 QUnit.module('Fastboot render');
 
 QUnit.test('it renders', function(assert) {
-  var done = assert.async();
   assert.expect(3);
 
   return request('http://localhost:3000').then(function(response) {
     assert.equal(response.statusCode, 200, 'Request is successful');
     assert.equal(response.headers["content-type"], "text/html; charset=utf-8", 'Content type is correct');
-    jsdom.env(response.body, function(err, window) {
-      var eps = window.document.querySelector('.ember-power-select');
-      assert.ok(eps, 'The select has been rendered');
-      done();
-    });
+    var document = jsdom(response.body).defaultView.document;
+    var eps = document.querySelector('.ember-power-select');
+    assert.ok(eps, 'The select has been rendered');
   });
 });
