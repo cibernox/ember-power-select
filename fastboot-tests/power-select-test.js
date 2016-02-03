694be59ed666b7c4238441e4bed4cfdf9089164a
cibernox <miguel.camba@gmail.com>
2016-02-03 11:07:22 +0000

    It works!

--- fastboot-tests/power-select-test.js
+++ fastboot-tests/power-select-test.js
@@ -1,15 +1,20 @@
 var RSVP    = require('rsvp');
 var request = RSVP.denodeify(require('request'));
+var jsdom   = require("jsdom");
 
 QUnit.module('Fastboot render');
 
 QUnit.test('it renders', function(assert) {
-  assert.expect(1);
+  var done = assert.async();
+  assert.expect(3);
 
   return request('http://localhost:3000').then(function(response) {
-    console.log(response.body);
     assert.equal(response.statusCode, 200, 'Request is successful');
     assert.equal(response.headers["content-type"], "text/html; charset=utf-8", 'Content type is correct');
-    assert.equal(response.body);
+    jsdom.env(response.body, function(err, window) {
+      var eps = window.document.querySelector('.ember-power-select');
+      assert.ok(eps, 'The select has been rendered');
+      done();
+    });
   });
 });
