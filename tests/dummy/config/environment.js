250ec60010ad9e9deb7605be4e2339de73002e42
cibernox <miguel.camba@gmail.com>
2016-02-05 16:09:37 +0000

    Update to lastest ember-cli

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -16,16 +16,6 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    },
-
-    contentSecurityPolicy: {
-      'default-src': "'none'",
-      'script-src': "'self'",
-      'font-src': "'self' https://fonts.gstatic.com",
-      'connect-src': "'self' http://api.github.com https://api.github.com",
-      'img-src': "'self' data:",
-      'style-src': "'self' 'unsafe-inline' https://fonts.googleapis.com",
-      'media-src': "'self'"
     }
   };
 
