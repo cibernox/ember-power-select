c0748de6c4cbee4bffb48ab0bbc7a618c876a41c
cibernox <miguel.camba@gmail.com>
2016-03-16 10:11:54 +0000

    Add bootstrap live example

---
+++ tests/dummy/app/controllers/public-pages/cookbook/bootstrap-theme.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+const countries = [
+  { name: 'United States',  flagUrl: '/flags/us.svg' },
+  { name: 'Spain',          flagUrl: '/flags/es.svg' },
+  { name: 'Portugal',       flagUrl: '/flags/pt.svg' },
+  { name: 'Russia',         flagUrl: '/flags/ru.svg' },
+  { name: 'Latvia',         flagUrl: '/flags/lv.svg' },
+  { name: 'Brazil',         flagUrl: '/flags/br.svg' },
+  { name: 'United Kingdom', flagUrl: '/flags/gb.svg' },
+];
+
+export default Ember.Controller.extend({
+  options: countries,
+  selectedSingle: null,
+  selectedMultiple: [],
+});
