ae0cfbae9d8d350d55557eecb0c596cc228394e1
cibernox <miguel.camba@gmail.com>
2016-03-17 09:39:55 +0000

    Remove power-select-multiple/options component.
    The generic one is enough now that how the new selection is build
    it done by a passed in function.

--- addon/components/power-select-multiple/options.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import layout from '../../templates/components/power-select/options';
-
-export default Ember.Component.extend({
-  layout: layout,
-  tagName: 'ul',
-  attributeBindings: ['role', 'aria-controls'],
-  role: 'listbox'
-});
