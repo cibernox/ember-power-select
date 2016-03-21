ccf343767af96c59e8efaab792111ce06642c4b2
cibernox <miguel.camba@gmail.com>
2016-03-21 12:51:29 +0000

    Improve ember-data testing

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -1,6 +1,6 @@
 export default function() {
 
-  this.timing = 4000;
+  this.timing = 2000;
   this.get('/users');
 
   this.passthrough('http://api.github.com/search/repositories');
--- tests/integration/components/power-select/ember-data-test.js
+++ tests/integration/components/power-select/ember-data-test.js
@@ -1,24 +1,27 @@
 import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
+import wait from 'ember-test-helpers/wait';
 import hbs from 'htmlbars-inline-precompile';
 import { typeInSearch, clickTrigger } from '../../../helpers/ember-power-select';
-import mirageInitializer from '../../../../initializers/ember-cli-mirage';
+import { startMirage } from '../../../../initializers/ember-cli-mirage';
 import emberDataInitializer from '../../../../initializers/ember-data';
 
 moduleForComponent('ember-power-select', 'Integration | Component | Ember Power Select (Ember-data integration)', {
   integration: true,
   beforeEach() {
     let owner = Ember.getOwner(this);
-    mirageInitializer.initialize(owner);
+    this.server = startMirage();
+
     emberDataInitializer.initialize(owner);
     this.store = owner.lookup('service:store');
+  },
+
+  afterEach() {
+    this.server.shutdown();
   }
 });
 
 test('Passing as options of a `store.findAll` works', function(assert) {
-  let done = assert.async();
-  server.createList('user', 10);
-
   this.users = this.store.findAll('user');
   this.render(hbs`
     {{#power-select options=users searchField="name" onchange=(action (mut foo)) as |option|}}
@@ -29,17 +32,14 @@ test('Passing as options of a `store.findAll` works', function(assert) {
   clickTrigger();
   assert.equal($('.ember-power-select-option').text().trim(), 'Loading options...', 'The loading message appears while the promise is pending');
 
-  setTimeout(function() {
+  return wait().then(function() {
     assert.equal($('.ember-power-select-option').length, 10, 'Once the collection resolves the options render normally');
     typeInSearch('2');
     assert.equal($('.ember-power-select-option').length, 1, 'Filtering works');
-    done();
-  }, 10);
+  });
 });
 
 test('Passing as options the result of `store.query` works', function(assert) {
-  let done = assert.async();
-  server.createList('user', 10);
   this.users = this.store.query('user', { foo: 'bar' });
   this.render(hbs`
     {{#power-select options=users searchField="name" onchange=(action (mut foo)) as |option|}}
@@ -50,10 +50,9 @@ test('Passing as options the result of `store.query` works', function(assert) {
   clickTrigger();
   assert.equal($('.ember-power-select-option').text().trim(), 'Loading options...', 'The loading message appears while the promise is pending');
 
-  setTimeout(function() {
+  return wait().then(function() {
     assert.equal($('.ember-power-select-option').length, 10, 'Once the collection resolves the options render normally');
     typeInSearch('2');
     assert.equal($('.ember-power-select-option').length, 1, 'Filtering works');
-    done();
-  }, 10);
+  });
 });
