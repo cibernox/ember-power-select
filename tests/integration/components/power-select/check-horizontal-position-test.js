ec770599cb9d1901f4734f2771c5f9fe356387e9
Pasikonik <blind.x6@gmail.com>
2016-04-03 17:32:13 +0200

    add test for checking hotizontal position

---
+++ tests/integration/components/power-select/check-horizontal-position-test.js
@@ -0,0 +1,35 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import { clickTrigger } from '../../../helpers/ember-power-select';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('ember-power-select', 'Integration | Component | Ember Power Select (horizontalPosition)', {
+  integration: true
+});
+
+test('check horizontal position without property', function(assert) {
+  assert.expect(2);
+
+  this.render(hbs`
+    {{#power-select horizontalPosition="right" onchange=(action (mut foo)) as |option|}}
+      {{option}}
+    {{/power-select}}
+  `);
+
+  clickTrigger();
+  assert.equal($('.ember-basic-dropdown--left').length, 0);;
+  assert.equal($('.ember-basic-dropdown--right').length, 2);
+});
+
+test('check horizontal position with property', function(assert) {
+  assert.expect(2);
+
+  this.render(hbs`
+    {{#power-select onchange=(action (mut foo)) as |option|}}
+      {{option}}
+    {{/power-select}}
+  `);
+
+  clickTrigger();
+  assert.equal($('.ember-basic-dropdown--right').length, 0);
+  assert.equal($('.ember-basic-dropdown--left').length, 2);
+});
