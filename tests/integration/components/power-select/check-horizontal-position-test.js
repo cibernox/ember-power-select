902a4c9cdf4cb3c08e06f00eae5aa779282f36eb
Pasikonik <blind.x6@gmail.com>
2016-04-08 18:25:43 +0200

    add test for multiple component

--- tests/integration/components/power-select/check-horizontal-position-test.js
+++ tests/integration/components/power-select/check-horizontal-position-test.js
@@ -1,12 +1,13 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import { clickTrigger } from '../../../helpers/ember-power-select';
 import hbs from 'htmlbars-inline-precompile';
+import { groupedNumbers } from '../constants';
 
 moduleForComponent('ember-power-select', 'Integration | Component | Ember Power Select (horizontalPosition)', {
   integration: true
 });
 
-test('check horizontal position specifying `right`', function(assert) {
+test('check horizontal position specifying `right` (using power-select component)', function(assert) {
   assert.expect(2);
 
   this.render(hbs`
@@ -20,13 +21,29 @@ test('check horizontal position specifying `right`', function(assert) {
   assert.equal($('.ember-basic-dropdown--right').length, 2);
 });
 
-test('check horizontal position without specify any property', function(assert) {
+test('check horizontal position without specify property (using power-select component)', function(assert) {
   assert.expect(2);
 
   this.render(hbs`
     {{#power-select onchange=(action (mut foo)) as |option|}}
       {{option}}
     {{/power-select}}
+  `);
+
+  clickTrigger();
+  assert.equal($('.ember-basic-dropdown--right').length, 0);
+  assert.equal($('.ember-basic-dropdown--left').length, 2);
+});
+
+
+test('check horizontal position without specify property (using power-select-multiple component)', function(assert) {
+  assert.expect(2);
+
+  this.groupedNumbers = groupedNumbers;
+  this.render(hbs`
+    {{#power-select-multiple options=groupedNumbers onchange=(action (mut foo)) as |option|}}
+      {{option}}
+    {{/power-select-multiple}}
   `);
 
   clickTrigger();
