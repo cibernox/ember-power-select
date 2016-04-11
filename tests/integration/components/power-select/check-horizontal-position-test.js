8cb1ed08cbb8f98d8afc2cba0e6e3ee2b9bd8e99
Pasikonik <blind.x6@gmail.com>
2016-04-11 18:15:01 +0200

    add test to check horizontal position for multiple component

--- tests/integration/components/power-select/check-horizontal-position-test.js
+++ tests/integration/components/power-select/check-horizontal-position-test.js
@@ -50,3 +50,18 @@ test('check horizontal position without specify property (using power-select-mul
   assert.equal($('.ember-basic-dropdown--right').length, 0);
   assert.equal($('.ember-basic-dropdown--left').length, 2);
 });
+
+test('check horizontal position with specify property (using power-select-multiple component)', function(assert) {
+  assert.expect(2);
+
+  this.groupedNumbers = groupedNumbers;
+  this.render(hbs`
+    {{#power-select-multiple options=groupedNumbers horizontalPosition="right" onchange=(action (mut foo)) as |option|}}
+      {{option}}
+    {{/power-select-multiple}}
+  `);
+
+  clickTrigger();
+  assert.equal($('.ember-basic-dropdown--right').length, 2);
+  assert.equal($('.ember-basic-dropdown--left').length, 0);
+});
