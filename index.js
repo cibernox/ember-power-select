0c5bba619f6f67a26eff2bd84b7f12de8ae0f648
Richard Livsey <richard@livsey.org>
2016-07-21 15:22:18 +0100

    Allow disabling styles when not using Sass
    var app = new EmberApp(defaults, {
      'ember-power-select': {
        theme: false
      }
    });

--- index.js
+++ index.js
@@ -9,19 +9,23 @@ module.exports = {
     if (!app.__emberPowerSelectIncludedInvoked) {
       app.__emberPowerSelectIncludedInvoked = true;
 
+      var addonConfig = app.options['ember-power-select'] || {};
+
       // Since ember-power-select styles already `@import` styles of ember-basic-dropdown,
-      // this flag tells to ember-basic-dropdown to skip importing its styles.
-      app.__skipEmberBasicDropdownStyles = true;
+      // this flag tells to ember-basic-dropdown to skip importing its styles provided
+      // we're using a theme (or the default styles)
+      if (addonConfig.theme !== false) {
+        app.__skipEmberBasicDropdownStyles = true;
+      }
 
       this._super.included.apply(this, arguments);
+
       // Don't include the precompiled css file if the user uses ember-cli-sass
       if (!app.registry.availablePlugins['ember-cli-sass']) {
-        var addonConfig = app.options['ember-power-select'];
-
-        if (!addonConfig || !addonConfig.theme) {
-          app.import('vendor/ember-power-select.css');
-        } else {
+        if (addonConfig.theme) {
           app.import('vendor/ember-power-select-'+addonConfig.theme+'.css');
+        } else if (addonConfig.theme !== false) {
+          app.import('vendor/ember-power-select.css');
         }
       }
     }
