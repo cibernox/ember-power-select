c124cd05410a58881bcd55fafc95a1b4325a6d17
cibernox <miguel.camba@gmail.com>
2016-10-03 22:54:46 +0100

    Save progress on material theme

--- tests/dummy/app/controllers/public-pages/cookbook.js
+++ tests/dummy/app/controllers/public-pages/cookbook.js
@@ -9,6 +9,7 @@ const groupedSections = [
     options: [
       { route: 'public-pages.cookbook.index',                 text: 'System-wide config' },
       { route: 'public-pages.cookbook.bootstrap-theme',       text: 'Bootstrap theme' },
+      { route: 'public-pages.cookbook.material-theme',        text: 'Material theme' },
       { route: 'public-pages.cookbook.css-animations',        text: 'CSS animations' },
       { route: 'public-pages.cookbook.debounce-searches',     text: 'Debounce searches' },
       { route: 'public-pages.cookbook.create-custom-options', text: 'Create custom options' }
