b05e92679a26dc2c46159b8bad07a49cb756dcf8
cibernox <miguel.camba@gmail.com>
2017-01-26 13:34:09 +0000

    [FEATURE] Allow to customise trigger’s id
    Just pass `triggerId=“foo”`

--- tests/integration/components/power-select/customization-with-attrs-test.js
+++ tests/integration/components/power-select/customization-with-attrs-test.js
@@ -3,7 +3,7 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { countries } from '../constants';
 
-moduleForComponent('ember-power-select', 'Integration | Component | Ember Power Select (Customization using triggerClass)', {
+moduleForComponent('ember-power-select', 'Integration | Component | Ember Power Select (Customization using attrs)', {
   integration: true
 });
 
@@ -35,4 +35,34 @@ test('trigger on multiple selects can be customized using triggerClass', functio
   `);
 
   assert.equal($('.country-multiple-trigger').length, 1, 'Class was added.');
+});
+
+test('Trigger can have a custom id passing triggerId', function(assert) {
+  assert.expect(1);
+
+  this.countries = countries;
+  this.country = [countries[1], countries[0]];
+
+  this.render(hbs`
+    {{#power-select options=countries selected=country onchange=(action (mut foo)) triggerId="this-is-my-id" as |country|}}
+      {{country.name}}
+    {{/power-select}}
+  `);
+
+  assert.equal($('.ember-power-select-trigger').attr('id'), 'this-is-my-id', 'The `id` was added.');
+});
+
+test('Trigger can have a custom id passing triggerId', function(assert) {
+  assert.expect(1);
+
+  this.countries = countries;
+  this.country = [countries[1], countries[0]];
+
+  this.render(hbs`
+    {{#power-select-multiple options=countries selected=country onchange=(action (mut foo)) triggerId="this-is-my-id" as |country|}}
+      {{country.name}}
+    {{/power-select-multiple}}
+  `);
+
+  assert.equal($('.ember-power-select-trigger').attr('id'), 'this-is-my-id', 'The `id` was added.');
 });
