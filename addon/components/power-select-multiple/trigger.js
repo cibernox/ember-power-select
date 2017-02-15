828580621c92c28e65e10825030f38e14e586b61
Benjamin Schönburg <lazybensch@gmail.com>
2017-02-15 14:16:38 +0100

    fix jscs

--- addon/components/power-select-multiple/trigger.js
+++ addon/components/power-select-multiple/trigger.js
@@ -70,8 +70,8 @@ export default Component.extend({
   }),
 
   maybePlaceholder: computed('placeholder', 'select.selected.length', function() {
-    const component = this.get('placeholderComponent');
-    const hasCustomPlaceholder = component !== 'power-select/placeholder';
+    let component = this.get('placeholderComponent');
+    let hasCustomPlaceholder = component !== 'power-select/placeholder';
 
     if (isIE || hasCustomPlaceholder) {
       return null;
