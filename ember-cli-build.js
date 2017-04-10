ae2c732394618f812519e2f76b1ca39d8d634466
cibernox <miguel.camba@gmail.com>
2017-04-10 20:35:13 +0100

    Update to 2.13 beta

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
-/* global require, module */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -7,16 +7,13 @@ const merge = Ember.assign || Ember.merge;
 registerPowerSelectHelpers();
 
 export default function startApp(attrs) {
-  let application;
-
   let attributes = merge({}, config.APP);
   attributes = merge(attributes, attrs); // use defaults, but you can override;
 
-  Ember.run(() => {
-    application = Application.create(attributes);
+  return Ember.run(() => {
+    let application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
+    return application;
   });
-
-  return application;
 }
