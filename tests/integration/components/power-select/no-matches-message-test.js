ea2d48c52c92a395305d3bea362c786ce3f063a6
Nathan <nlincoln@intellifarms.com>
2017-05-28 14:43:05 -0500

    Adds blank test for added component

---
+++ tests/integration/components/power-select/no-matches-message-test.js
@@ -0,0 +1,6 @@
+import { moduleForComponent } from 'ember-qunit';
+// import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('power-select/no-matches-message', 'Integration | Component | power select/no matches message', {
+  integration: true
+});
