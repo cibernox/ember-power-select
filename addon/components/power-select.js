a561da2b031a4d82bacf4ec16d42a7bc6f4d9416
Donald Wasserman <djwasserman@gmail.com>
2018-04-19 16:31:59 -0400

    fixes #1094

--- addon/components/power-select.js
+++ addon/components/power-select.js
@@ -138,7 +138,7 @@ export default Component.extend({
       return null;
     },
     set(_, selected) {
-      if (selected && selected.then) {
+      if (selected && get(selected, 'then')) {
         this.get('_updateSelectedTask').perform(selected);
       } else {
         scheduleOnce('actions', this, this.updateSelection, selected);
