8d56b76e2792f85b47836df8e72297b580ace718
billybonks <sebastienstettler@gmail.com>
2018-04-26 22:42:24 +0800

    add active descendent

--- addon/components/power-select.js
+++ addon/components/power-select.js
@@ -169,6 +169,18 @@ export default Component.extend({
     }
   }),
 
+  optionIds: computed('options.[]','publicAPI.optionsId', function() {
+    debugger
+    return this.get('options').reduce((acc, option, index) => {
+      acc[option]=`${this.get('optionsId')}-option-${index}`;
+      return acc;
+    }, {});
+  }),
+
+  activeDescendant: computed('optionIds', 'publicAPI.highlighted', function() {
+    return this.get('optionIds')[this.get('publicAPI.highlighted')];
+  }),
+
   optionMatcher: optionsMatcher('matcher', defaultMatcher),
 
   typeAheadOptionMatcher: optionsMatcher('typeAheadMatcher', defaultTypeAheadMatcher),
