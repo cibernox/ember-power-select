5880b011228f284512b4b3d631acfbabc4c17b00
Miguel Camba <miguel.camba@gmail.com>
2018-07-07 20:54:23 +0200

    Update to ember-cli 3.2 (#1132)

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
         'ember-cli-build.js',
         'index.js',
         'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'tests/dummy/config/**/*.js'
       ],
