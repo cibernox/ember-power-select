3832f1db519cd75be327d01ee3a254f2cad28179
Miguel Camba <miguel.camba@gmail.com>
2019-06-09 14:14:03 +0200

    Remove unsupported versions of ember

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,26 +12,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-3.4',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.4.0'
-            }
-          }
-        },
-        {
           name: 'ember-release',
           npm: {
             devDependencies: {
