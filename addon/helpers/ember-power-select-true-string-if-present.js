1a73ecc7700e863234dbb7b32b77c7c3fd543eaa
Miguel Camba <miguel.camba@gmail.com>
2019-06-12 11:23:17 +0200

    Remove helper that is not needed anymore

--- addon/helpers/ember-power-select-true-string-if-present.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function emberPowerSelectTrueStringIfPresent([bool]/* , hash*/) {
-  return bool ? 'true' : false;
-}
-
-export default helper(emberPowerSelectTrueStringIfPresent);
