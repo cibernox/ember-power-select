7f9a2aac7c9249bec5c565287385883014480559
Miguel Camba <miguel.camba@gmail.com>
2019-09-04 23:58:24 +0200

    Convert power-select/placeholder to a template-only glimmer component

--- addon/components/power-select/placeholder.js
+++
@@ -1,7 +0,0 @@
-import { layout, tagName } from "@ember-decorators/component";
-import Component from '@ember/component';
-import templateLayout from '../../templates/components/power-select/placeholder';
-
-export default @tagName('') @layout(templateLayout) class Placeholder extends Component {
-
-}
