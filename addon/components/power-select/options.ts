d93dd40cabb1752ac34963bd07fd9dad3bcbbb5f
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2020-10-17 00:22:54 +0300

    Comment about distance

--- addon/components/power-select/options.ts
+++ addon/components/power-select/options.ts
@@ -130,6 +130,7 @@ export default class Options extends Component<Args> {
       return true;
     }
 
+    // Distinguish stylus scroll and tap: if touch "distance" < 5px, we consider it a tap
     let horizontalDistance = Math.abs(moveEvent.changedTouches[0].pageX - endEvent.changedTouches[0].pageX);
     let verticalDistance = Math.abs(moveEvent.changedTouches[0].pageY - endEvent.changedTouches[0].pageY);
     return horizontalDistance >= 5 || verticalDistance >= 5;
