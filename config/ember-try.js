4fee8269855764730ed68b2f8cd0a8dd239daf49
Miguel Camba <miguel.camba@gmail.com>
2021-12-06 20:24:28 +0100

    Add embroider scenario

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe } = require('@embroider/test-setup');
 
 module.exports = async function() {
   return {
@@ -59,6 +60,7 @@ module.exports = async function() {
           devDependencies: {}
         }
       },
+      embroiderSafe()
     ]
   };
 };
