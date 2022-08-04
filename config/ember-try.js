530c6b98b92c56eb30c245b21a3c96095d9b7321
Chris Manson <chris@manson.ie>
2022-08-04 13:53:01 +0100

    add back ember-try scenarios that were removed

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,6 +7,24 @@ module.exports = async function () {
   return {
     scenarios: [
       {
+        name: 'ember-lts-3.16',
+
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.16.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.20',
+
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.0',
+          },
+        },
+      },
+      {
         name: 'ember-lts-3.24',
 
         npm: {
