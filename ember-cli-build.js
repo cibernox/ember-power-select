4834730b28c470090fc589f0ecf99d85d2834645
Adam Woźny <wozny1989@gmail.com>
2022-09-19 18:10:43 +0200

    chore: snippet improvements

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     snippetPaths: ['tests/dummy/app/templates/snippets/'],
+    'ember-prism': {
+      components: ['scss', 'javascript'], //needs to be an array, or undefined.
+    },
   });
 
   /*
