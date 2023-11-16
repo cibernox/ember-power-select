3f03fd131fc1e7189b3661ba74c9e4e6e4ff1bc6
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-11-16 14:12:33 +0100

    Add ts-expect-error to ignore error

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,6 @@
 // Types for compiled templates
 declare module 'ember-power-select/templates/*' {
+  // @ts-expect-error
   import { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
