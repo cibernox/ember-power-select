5bcea403259ce674dbc65b9205ef2791258a3fe5
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-12-07 08:57:20 +0100

    Fix lint

--- tests/dummy/app/controllers/public-pages/cookbook/debounce-searches.js
+++ tests/dummy/app/controllers/public-pages/cookbook/debounce-searches.js
@@ -2,7 +2,6 @@ import Controller from '@ember/controller';
 import { isBlank } from '@ember/utils';
 import { debounce } from '@ember/runloop';
 import RSVP from 'rsvp';
-import { task, timeout } from 'ember-concurrency';
 import DebounceSearches1 from '../../../components/snippets/debounce-searches-1';
 import DebounceSearches2 from '../../../components/snippets/debounce-searches-2';
 
@@ -15,15 +14,6 @@ export default class extends Controller {
       debounce(_performSearch, term, resolve, reject, 600);
     });
   }
-
-  @task(function* (term) {
-    yield timeout(600);
-    let url = `https://api.github.com/search/repositories?q=${term}`;
-    return fetch(url)
-      .then((resp) => resp.json())
-      .then((json) => json.items);
-  })
-  searchRepo;
 }
 
 function _performSearch(term, resolve, reject) {
