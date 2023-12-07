bcffe5fb28053dccf149817c9f2f738e5d2bed23
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-12-07 08:59:10 +0100

    Remove code example from controller

--- tests/dummy/app/controllers/public-pages/cookbook/debounce-searches.js
+++ tests/dummy/app/controllers/public-pages/cookbook/debounce-searches.js
@@ -1,27 +1,8 @@
 import Controller from '@ember/controller';
-import { isBlank } from '@ember/utils';
-import { debounce } from '@ember/runloop';
-import RSVP from 'rsvp';
 import DebounceSearches1 from '../../../components/snippets/debounce-searches-1';
 import DebounceSearches2 from '../../../components/snippets/debounce-searches-2';
 
 export default class extends Controller {
   debounceSearches1 = DebounceSearches1;
   debounceSearches2 = DebounceSearches2;
-
-  searchRepo(term) {
-    return new RSVP.Promise((resolve, reject) => {
-      debounce(_performSearch, term, resolve, reject, 600);
-    });
-  }
-}
-
-function _performSearch(term, resolve, reject) {
-  if (isBlank(term)) {
-    return resolve([]);
-  }
-  let url = `https://api.github.com/search/repositories?q=${term}`;
-  fetch(url)
-    .then((resp) => resp.json())
-    .then((json) => resolve(json.items), reject);
 }
