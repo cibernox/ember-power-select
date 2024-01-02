6647bc1975d9efb2005452802a9200d573d6d8be
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-01-02 07:00:48 +0100

    Readd blueprint

--- ember-power-select/rollup.config.mjs
+++ ember-power-select/rollup.config.mjs
@@ -74,6 +74,7 @@ export default [
       // "app" tree. Things in here should also be in publicEntrypoints above, but
       // not everything in publicEntrypoints necessarily needs to go here.
       addon.appReexports([
+        'blueprints/**/*.js',
         'components/**/*.js',
         'helpers/**/*.js',
         'test-support/*.js',
