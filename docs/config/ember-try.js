5889b14a4d0f2b732368ec9637445db9525c93f0
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-01-17 10:21:03 +0100

    Rename ember-5.4 to ember-lts-5.4 in ember-try

--- docs/config/ember-try.js
+++ docs/config/ember-try.js
@@ -45,7 +45,7 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-5.4',
+        name: 'ember-lts-5.4',
         npm: {
           devDependencies: {
             'ember-source': '~5.4.0',
