2636ef2f948ec281c2bc5d623e144afe899228c6
Adam Woźny <wozny1989@gmail.com>
2024-06-26 10:59:15 +0200

    docs: remove test helper setup (#1842)

--- docs/app/templates/snippets/test-helpers-1-js.js
+++
@@ -1,9 +0,0 @@
-import Application from '../../app';
-import config from '../../config/environment';
-import registerPowerSelectHelpers from 'ember-power-select/test-support/helpers';
-
-registerPowerSelectHelpers();
-
-export default function startApp(attrs) {
-  //...
-}
