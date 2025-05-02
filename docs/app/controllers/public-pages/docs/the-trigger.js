17935c7b851db1edffa5be82c7f713402a2e592a
Andrew Paterson <andrew.paterson9@gmail.com>
2025-05-02 13:45:37 +0200

    Update docs

--- docs/app/controllers/public-pages/docs/the-trigger.js
+++ docs/app/controllers/public-pages/docs/the-trigger.js
@@ -4,6 +4,7 @@ import TheTrigger2 from '../../../components/snippets/the-trigger-2';
 import TheTrigger3 from '../../../components/snippets/the-trigger-3';
 import TheTrigger4 from '../../../components/snippets/the-trigger-4';
 import TheTrigger5 from '../../../components/snippets/the-trigger-5';
+import TheTrigger6 from '../../../components/snippets/the-trigger-6';
 
 export default class TheTrigger extends Controller {
   theTrigger1 = TheTrigger1;
@@ -11,4 +12,5 @@ export default class TheTrigger extends Controller {
   theTrigger3 = TheTrigger3;
   theTrigger4 = TheTrigger4;
   theTrigger5 = TheTrigger5;
+  theTrigger6 = TheTrigger6;
 }
