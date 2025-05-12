2c88946846a5afb167fd416b796751703c525299
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-05-12 09:42:15 +0200

    Run format in docs

--- docs/app/components/snippets/debounce-searches-2.js
+++ docs/app/components/snippets/debounce-searches-2.js
@@ -2,11 +2,12 @@ import Component from '@glimmer/component';
 import { task, timeout } from 'ember-concurrency';
 
 export default class extends Component {
-  @(task(function* (term) {
+  @task(function* (term) {
     yield timeout(600);
     let url = `https://api.github.com/search/repositories?q=${term}`;
     return fetch(url)
       .then((resp) => resp.json())
       .then((json) => json.items);
-  })) searchRepo;
+  })
+  searchRepo;
 }
