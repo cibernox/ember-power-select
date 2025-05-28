34ec0d09fa9f3587a7a5ac05d444d6acef59249b
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-05-28 20:39:43 +0200

    Add check of isOpen in handleBlur

--- ember-power-select/src/components/power-select/input.ts
+++ ember-power-select/src/components/power-select/input.ts
@@ -47,7 +47,7 @@ export default class PowerSelectInput extends Component<PowerSelectInputSignatur
 
   @action
   handleBlur(event: Event) {
-    if (this.args.searchFieldPosition === 'trigger') {
+    if (!this.args.select.isOpen && this.args.searchFieldPosition === 'trigger') {
       this.args.select.actions?.search('');
     }
 
