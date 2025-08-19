77a6b335cb93e5169c93d4513afc70d9ff26fb97
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-08-19 07:41:31 +0200

    Fix ember-concurrency usages in docs (replace `@task`) (#1935)

--- docs/app/components/snippets/custom-search-action-2.js
+++ docs/app/components/snippets/custom-search-action-2.js
@@ -2,12 +2,11 @@ import Component from '@glimmer/component';
 import { task, timeout } from 'ember-concurrency';
 
 export default class extends Component {
-  @task(function* (term) {
-    yield timeout(1500);
+  searchTask = task(async (term) => {
+    await timeout(1500);
     let url = `https://api.github.com/search/repositories?q=${term}`;
     return fetch(url)
       .then((resp) => resp.json())
       .then((json) => json.items);
-  })
-  searchTask;
+  });
 }
