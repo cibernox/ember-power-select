ee392cb49cc16620495fecfb9e82a2d9a4836d15
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-08-29 20:27:43 +0200

    Convert customization-with-extending-components-test to TS

--- test-app/app/components/custom-before-options-two.ts
+++ test-app/app/components/custom-before-options-two.ts
@@ -1,6 +1,6 @@
 import PowerSelectBeforeOptionsComponent from 'ember-power-select/components/power-select/before-options';
 import type { SelectedCountryExtra } from 'test-app/utils/constants';
 
-export default class CustomBeforeOptions<
+export default class CustomBeforeOptionsTwo<
   T,
 > extends PowerSelectBeforeOptionsComponent<T, SelectedCountryExtra> {}
