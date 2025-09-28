552dba469ad1a9fd9d8e7db7e65e6e4a959bd6e4
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-09-28 20:00:16 +0200

    Add multiple logic into single & add `@multiple` parameter

--- ember-power-select/src/components/power-select/input.ts
+++ ember-power-select/src/components/power-select/input.ts
@@ -1,10 +1,14 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { modifier } from 'ember-modifier';
-import { task, timeout } from 'ember-concurrency';
+import { assert, deprecate } from '@ember/debug';
+import { isBlank } from '@ember/utils';
+import { get } from '@ember/object';
 import type { Select, TSearchFieldPosition } from '../power-select';
+import { task, timeout } from 'ember-concurrency';
+import type { ComponentLike } from '@glint/template';
 
-interface PowerSelectInputSignature {
+export interface PowerSelectInputSignature {
   Element: HTMLElement;
   Args: {
     select: Select;
@@ -12,15 +16,21 @@ interface PowerSelectInputSignature {
     ariaLabelledBy?: string;
     ariaDescribedBy?: string;
     role?: string;
+    searchField?: string;
     searchPlaceholder?: string;
+    placeholder?: string;
     searchFieldPosition?: TSearchFieldPosition;
     ariaActiveDescendant?: string;
+    isDefaultPlaceholder?: boolean;
     listboxId?: string;
-    onKeydown: (e: KeyboardEvent) => false | void;
-    onBlur: (e: FocusEvent) => void;
-    onFocus: (e: FocusEvent) => void;
-    onInput: (e: InputEvent) => boolean;
     autofocus?: boolean;
+    tabindex?: number | string;
+    placeholderComponent?: string | ComponentLike<any>;
+    onKeydown?: (e: KeyboardEvent) => boolean | void;
+    onBlur?: (e: FocusEvent) => void;
+    onFocus?: (e: FocusEvent) => void;
+    onInput?: (e: InputEvent) => void | boolean;
+    buildSelection: (lastSelection: any, select: Select) => any[];
   };
 }
 
@@ -29,12 +39,79 @@ export default class PowerSelectInput extends Component<PowerSelectInputSignatur
 
   private _lastIsOpen: boolean = this.args.select.isOpen;
 
+  get placeholder() {
+    if (this.args.placeholder !== undefined) {
+      deprecate(
+        'You are using power-select::input-field with parameter @placeholder. Replace @placeholder with @searchPlaceholder',
+        false,
+        {
+          for: 'ember-power-select',
+          id: 'ember-power-select.deprecate-input-field-placeholder-argument',
+          since: {
+            enabled: '8.10',
+            available: '8.10',
+          },
+          until: '9.0.0',
+        },
+      );
+    }
+
+    if (!this.args.isDefaultPlaceholder) {
+      return undefined;
+    }
+
+    if (this.args.select.multiple) {
+      return !this.args.select.selected ||
+        (Array.isArray(this.args.select.selected) &&
+          this.args.select.selected.length === 0)
+        ? this.args.placeholder || this.args.searchPlaceholder || ''
+        : '';
+    }
+
+    return this.args.placeholder || this.args.searchPlaceholder;
+  }
+
   @action
   handleKeydown(e: KeyboardEvent): false | void {
-    if (this.args.onKeydown(e) === false) {
+    if (e.target === null) return;
+    if (this.args.onKeydown && this.args.onKeydown(e) === false) {
+      e.stopPropagation();
       return false;
     }
-    if (e.keyCode === 13) {
+    if (this.args.select.multiple) {
+      if (e.keyCode === 8) {
+        e.stopPropagation();
+        if (
+          isBlank((e.target as HTMLInputElement).value) &&
+          this.args.buildSelection
+        ) {
+          const lastSelection =
+            Array.isArray(this.args.select.selected) &&
+            this.args.select.selected[this.args.select.selected.length - 1];
+          if (lastSelection) {
+            this.args.select.actions.select(
+              this.args.buildSelection(lastSelection, this.args.select),
+              e,
+            );
+            if (typeof lastSelection === 'string') {
+              this.args.select.actions.search(lastSelection);
+            } else {
+              assert(
+                '`<PowerSelect>` requires a `@searchField` when the options are not strings to remove options using backspace',
+                this.args.searchField,
+              );
+              this.args.select.actions.search(
+                (get(lastSelection, this.args.searchField) || '') as string,
+              );
+            }
+            this.args.select.actions.open(e);
+          }
+        }
+      } else if ((e.keyCode >= 48 && e.keyCode <= 90) || e.keyCode === 32) {
+        // Keys 0-9, a-z or SPACE
+        e.stopPropagation();
+      }
+    } else if (e.keyCode === 13) {
       this.args.select.actions.close(e);
     }
   }
@@ -42,9 +119,11 @@ export default class PowerSelectInput extends Component<PowerSelectInputSignatur
   @action
   handleInput(event: Event): false | void {
     const e = event as InputEvent;
-    if (this.args.onInput(e) === false) {
+    if (this.args.onInput && this.args.onInput(e) === false) {
       return false;
     }
+
+    this.args.select.actions.open(e);
   }
 
   @action
@@ -53,7 +132,16 @@ export default class PowerSelectInput extends Component<PowerSelectInputSignatur
       this.args.select.actions?.search('');
     }
 
-    this.args.onBlur(event as FocusEvent);
+    if (this.args.onBlur) {
+      this.args.onBlur(event as FocusEvent);
+    }
+  }
+
+  @action
+  handleFocus(event: Event) {
+    if (this.args.onFocus) {
+      this.args.onFocus(event as FocusEvent);
+    }
   }
 
   setupInput = modifier(
