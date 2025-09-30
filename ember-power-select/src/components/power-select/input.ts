5e610246666461c8acd64bec493b1528491c9b42
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-09-30 09:34:31 +0200

    Add check to avoid auto focus for input field in trigger (#1943)
    * Add check to avoid auto focus
    
    * Fix lint

--- ember-power-select/src/components/power-select/input.ts
+++ ember-power-select/src/components/power-select/input.ts
@@ -152,7 +152,12 @@ export default class PowerSelectInput extends Component<PowerSelectInputSignatur
 
       this.didSetup = true;
 
-      this._focusInput(el);
+      if (
+        this.args.searchFieldPosition === undefined ||
+        this.args.searchFieldPosition === 'before-options'
+      ) {
+        this._focusInput(el);
+      }
 
       return () => {
         this.args.select.actions?.search('');
