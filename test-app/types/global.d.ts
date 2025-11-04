de77ec73be9e029b2231f2fe9c36f312cf7b2c49
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-11-04 06:59:18 +0100

    Add EmberElementHelperRegistry

--- test-app/types/global.d.ts
+++ test-app/types/global.d.ts
@@ -3,11 +3,13 @@ import EmberPowerSelectRegistry from 'ember-power-select/template-registry';
 import type { EmbroiderUtilRegistry } from '@embroider/util';
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
 import type EmberConcurrencyRegistry from 'ember-concurrency/template-registry';
+import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry
     extends EmbroiderUtilRegistry,
       EmberPowerSelectRegistry,
       EmberTruthRegistry,
-      EmberConcurrencyRegistry {}
+      EmberConcurrencyRegistry,
+      EmberElementHelperRegistry {}
 }
