4df4dacb2161022dd72c7baf31feae07e30007c1
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-12-07 08:33:40 +0100

    Update ember-basic-dropdown to v8.9 (#1959)
    * Update ember-basic-dropdown to latest
    
    * Fix ember-basic-dropdown config deprecation warning

--- docs/app/app.ts
+++ docs/app/app.ts
@@ -3,12 +3,17 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+import { setConfig } from 'ember-basic-dropdown/config';
 
 import compatModules from '@embroider/virtual/compat-modules';
 
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
 }
+
+setConfig({
+  rootElement: config.APP['rootElement'] as string | undefined,
+});
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
