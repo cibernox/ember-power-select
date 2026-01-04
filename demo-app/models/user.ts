ea172afcf2a187f799633483d0152f4309992a93
Markus Sanin <markus.sanin@zeppelin-group.com>
2026-01-04 19:21:51 +0100

    Remove ember-data / miragejs (unused) (#1975)

--- demo-app/models/user.ts
+++
@@ -1,8 +0,0 @@
-// import Model, { attr, hasMany } from '@ember-data/model';
-// import type PetModel from './pet';
-
-// export default class UserModel extends Model {
-//   @attr('string') declare name: string;
-//   @attr('number') declare age: number;
-//   @hasMany('pet', { async: true, inverse: 'owner' }) declare pets: PetModel[];
-// }
--- docs/app/serializers/application.ts
+++
@@ -1,3 +0,0 @@
-import JSONAPISerializer from '@ember-data/serializer/json-api';
-
-export default class ApplicationSerializer extends JSONAPISerializer {}
