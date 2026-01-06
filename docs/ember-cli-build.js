83637184d780a686b6a3816b8f23452a8003f94a
Markus Sanin <markus.sanin@zeppelin-group.com>
2026-01-06 07:40:30 +0100

    Update ember v6.7.0...v6.9.1

--- docs/ember-cli-build.js
+++ docs/ember-cli-build.js
@@ -1,12 +1,10 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-
 const { compatBuild } = require('@embroider/compat');
 
 module.exports = async function (defaults) {
   const { buildOnce } = await import('@embroider/vite');
-
   let app = new EmberApp(defaults, {
     autoImport: {
       watchDependencies: ['ember-power-select'],
@@ -17,6 +15,7 @@ module.exports = async function (defaults) {
       ],
     },
     'ember-cli-babel': { enableTypeScriptTransform: true },
+    // Add options here
   });
 
   return compatBuild(app, buildOnce);
