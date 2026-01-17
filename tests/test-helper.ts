cb748a6afa26957a5c71e81afa0a4944c1bff73d
Markus Sanin <markus.sanin@zeppelin-group.com>
2026-01-17 19:25:54 +0100

    Prepare issue embroider macros

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -5,7 +5,6 @@ import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
 import { setConfig, type Config } from 'ember-basic-dropdown/config';
-// @ts-expect-error Could not find a declaration file for module '@embroider/macros/src/addon/runtime'.
 import { getGlobalConfig } from '@embroider/macros/src/addon/runtime';
 
 class Router extends EmberRouter {
@@ -24,6 +23,13 @@ class TestApp extends EmberApp {
 Router.map(function () {});
 
 export function start() {
+  //eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const theMacrosGlobal = getGlobalConfig();
+  //eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  theMacrosGlobal['@embroider/macros'] ||= {};
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  theMacrosGlobal['@embroider/macros'].isTesting ||= true;
+
   setApplication(
     TestApp.create({
       autoboot: false,
@@ -32,11 +38,6 @@ export function start() {
   );
   setup(QUnit.assert);
   setupEmberOnerrorValidation();
-  //eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-  const config = getGlobalConfig()['@embroider/macros'];
-
-  //eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  if (config) config.isTesting = true;
 
   setConfig(defaultBasicDropdownConfig);
 
@@ -48,8 +49,4 @@ export const defaultBasicDropdownConfig: Config = {
     import.meta.env.VITE_SHADOW_DOM_BUILD === 'true'
       ? '#ember-basic-dropdown-wormhole'
       : '#ember-testing',
-  destination:
-    import.meta.env.VITE_SHADOW_DOM_BUILD === 'true'
-      ? 'ember-basic-dropdown-wormhole'
-      : 'ember-testing', // Workaround, because embroider.isTesting isn't working
 };
